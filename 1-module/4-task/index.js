53f93b976e13b54f8453e21a6778110bb056daff
sacha <sacha.dorofeev1999@gmail.com>
2024-12-22 13:42:00 +0300

    домашняя работа 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let l = str[0].toLowerCase() + str.slice(1).toLowerCase();
+
+
+  if (l.includes("1xbet") || l.includes("xxx")) {
+    return true;
+  }
+  return false;
 }
