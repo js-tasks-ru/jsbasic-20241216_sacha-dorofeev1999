0fe0e76c58e9e5561a4c04f1a100f82fe9aa41e4
sacha <sacha.dorofeev1999@gmail.com>
2025-01-27 13:34:36 +0300

    homework6

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (i = 0; i < table.rows.length;i++) {
+    let td = table.rows[i].cells[i].style.background = "red";
+  }
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+  let trs = table.getElementsByTagName('tr');
+
+
+  for (i = 0; i < trs.length; i++) {
+    let cellAvaible = trs[i].cells[3];
+
+
+    if (cellAvaible.dataset.available === "true") {
+      trs[i].classList.add("available");
+    } else if (cellAvaible.dataset.available === "false") {
+      trs[i].classList.add("unavailable");
+    }
+
+    let cellFemale = trs[i].cells[2];
+
+    if (cellFemale.textContent == "m") {
+      trs[i].classList.add("male");
+    } else if (cellFemale.textContent == "f") {
+      trs[i].classList.add("female");
+    }
+
+    let cellAge = trs[i].cells[1];
+
+    if (Number(cellAge.textContent) < 18) {
+      trs[i].style.textDecoration = "line-through";
+    }
+
+    let cellHiden = trs[i].cells[3];
+
+    if (!cellHiden.hasAttribute("data-available")) {
+      trs[i].hidden = true;
+    }
+  }
 }
