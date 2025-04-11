6765fa29fb48d128be935ca0c841bce67f0fbd3d
sacha <sacha.dorofeev1999@gmail.com>
2025-04-11 22:30:28 +0300

    1

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,41 +2,73 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-    this.modal;
+    this.modalBlock();
     this.open();
     this.setTitle();
     this.setBody();
+    this.closeBtn();
+    this.closeEscape();
   }
 
-  open() {
-    this.modal = createElement(
-     `
-     <div class = "modal">
-       <div class="modal__overlay"></div>
+  modalBlock() {
+    this.elem = createElement(`
+      <div class="modal">
+      <div class="modal__overlay"></div>
       <div class="modal__inner">
         <div class="modal__header">
           <button type="button" class="modal__close">
             <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
           </button>
-        <h3 class="modal__title"></h3>
+          <h3 class="modal__title">
+          </h3>
         </div>
-        <div class="modal__body"></div>
+        <div class="modal__body">fsdfsdfsdsd
+        </div>
       </div>
-      </div>
-      `)
-    document.body.append(this.modal);
-    let body = document.querySelector("body")
-    body.classList.add("is-modal-open")
+    </div>
+      `);
+  }
+
+  open() {
+    document.body.classList.add("is-modal-open");
+    document.body.append(this.elem);
   }
 
   setTitle(title) {
-    this.modal = document.querySelector(".modal__title")
-    this.modal.innerText = title
+    let modalTitle = document.querySelector(".modal__title");
+
+
+    modalTitle.innerText = title;
   }
 
   setBody(node) {
-    this.modal = document.querySelector(".modal__body")
-    this.modal.innerHTML = ""
-    this.modal.append(node)
+    let modalBody = document.querySelector(".modal__body");
+
+
+    modalBody.innerHTML = "";
+    modalBody.append(node);
+  }
+
+  closeBtn() {
+    let modalClose = document.querySelector(".modal__close");
+
+
+    modalClose.addEventListener("click", (event) => {
+      this.close();
+    });
+  }
+
+  closeEscape() {
+    document.addEventListener("keydown", (event) => {
+      if(event.code === "Escape") {
+        this.close();
+      }
+    });
+  }
+
+  close(event) {
+    document.body.classList.remove("is-modal-open");
+    this.elem.remove();
+    document.removeEventListener("keydown", event);
 }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,70 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class StepSlider {
-  constructor({ steps, value = 0 }) {
+  constructor({ steps, value }) {
+    this.steps = steps;
+    this.value = value;
+    this.sliderBlock();
+    this.sliderChange();
+    this.addSlider();
+  }
+  sliderBlock() {
+    this.elem = createElement(`
+      <div class="container">
+  <div class="slider">
+    <div class="slider__thumb" style="left: 50%;">
+      <span class="slider__value">2</span>
+    </div>
+    <div class="slider__progress" style="width: 50%;"></div>
+    <div class="slider__steps">
+      <span class="slider__step-active"></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</div>
+  `);
+    document.body.append(this.elem);
+  }
+  sliderChange() {
+    let sliderDiv = document.querySelector(".slider");
+    let sliderValue = document.querySelector(".slider__value");
+    let sliderSteps = document.querySelector(".slider__steps");
+
+    sliderDiv.addEventListener("click", (event)=> {
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+      let left = event.clientX - this.elem.getBoundingClientRect().left;
+      let leftRelative = left / this.elem.offsetWidth;
+      let segments = this.steps - 1;
+      let approximateValue = leftRelative * 10;
+      this.value = Math.round(approximateValue);
+      let valuePercents = this.value / segments * 100;
+
+
+      thumb.style.left = `${valuePercents}%`;
+      progress.style.width = `${valuePercents}%`;
+      sliderValue.innerHTML = this.value;
+      if ("slider__step-active") {
+        for (let i = 0; i < this.steps; i++) {
+          sliderSteps.children[i].classList.remove("slider__step-active");
+        }
+      }
+      sliderSteps.children[this.value].classList.add("slider__step-active");
+
+      sliderDiv.append(thumb);
+      sliderDiv.append(progress);
+    });
+  }
+  addSlider() {
+    this.elem.addEventListener("click", (event) => {
+      this.elem.dispatchEvent(new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true
+      }));
+      console.log(event.detail)
+
+    });
   }
 }
